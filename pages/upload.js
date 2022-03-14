1397b29666dff0064c83aa2120a175ce61225e44
Tim Reber <tim.reber.1@postfinance.ch>
2022-03-14 11:17:15 +0100

    added upload site

--- pages/upload.js
+++ pages/upload.js
@@ -1,30 +1,42 @@
 import { storage } from "firebase/clientAPP"
+import { ref, uploadBytes  } from "firebase/storage"
+import { useState } from "react"
+
 
 export default function Test() {
 
+    const [image, setImage] = useState("hallo")
+    const upload = () => {
+        if (image == null) {
+            return
+        }
+        else {
+            const reference = ref(storage, `/images/${image.name}`)
+            uploadBytes(reference, image)
+        }
+    }
+    
     return (
-        <>
-            <form>
-                <div className="flex justify-center items-center h-screen">
-                    <div className="mb-3 w-96">
-                        <label htmlFor="formFile" className="form-label mb-2 text-gray-700 text-xl font-bold">Select your Picture</label>
-                        <input className="form-control
-                    block
-                    w-full
-                    text-base
-                    font-normal
-                    text-gray-700
-                    bg-white bg-clip-padding
-                    rounded
-                    transition
-                    ease-in-out
-                    mt-3
-                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile" />
-                    <button type="submit">Upload Image!</button>
-                    </div>
-                
-                </div>
-            </form>
-        </>
+        <div className="flex justify-center items-center h-screen">
+        <div className="mb-3 w-96">
+          <label htmlFor="formFileSm" className="form-label inline-block mb-2 text-gray-700">Select your file</label>
+          <input className="form-control
+          block
+          w-full
+          px-2
+          py-1
+          text-sm
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="formFileSm" type="file" onChange={(e) => {setImage(e.target.files[0])}}/>
+          <button onClick={upload}>Upload</button>
+        </div>
+      </div>
     )
 }
