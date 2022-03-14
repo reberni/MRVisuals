c5d38b06e8ab3668620c13afe394cb61fcf2f8e7
Tim Reber <tim.reber.1@postfinance.ch>
2022-03-14 15:22:26 +0100

    Turned iterate into component

---
+++ components/Iterate.js
@@ -0,0 +1,45 @@
+
+import { storage } from "firebase/clientAPP"
+import { ref, getDownloadURL, listAll } from "firebase/storage"
+import { useEffect, useState } from "react";
+
+export default function Iterate( { folder } ) {
+
+    const [links, setlinks] = useState([])
+    const [path, setPath] = useState([])
+    const testReference = ref(storage, folder)
+
+    useEffect(() => {
+        listAll(testReference).then(res => {
+            setlinks(res.items)
+            console.log(res.items)
+        })
+    }, [])
+
+
+    useEffect(() => {
+        if (links) getUrls()
+    }, [links])
+
+    const getUrls = async () => {
+        let urls = []
+        for (const imgPath of links) {
+            const pathReference = ref(storage, imgPath._location.path_)
+            const url = await getDownloadURL(pathReference)
+            urls.push(url)
+        }
+        setPath(urls)
+        console.log(urls)
+    }
+
+
+    return (
+        <>
+            {
+                path.map((url, i) => {
+                    return <img src={url} key={i} />
+                })
+            }
+        </>
+    )
+}
--- pages/test.js
+++ pages/test.js
@@ -1,45 +1,9 @@
+import Iterate from "components/Iterate";
 
-import { storage } from "firebase/clientAPP"
-import { ref, getDownloadURL, listAll } from "firebase/storage"
-import { useEffect, useState } from "react";
 
 export default function Test() {
-
-    const [links, setlinks] = useState([])
-    const [path, setPath] = useState([])
-    const testReference = ref(storage, "Susten")
-
-    useEffect(() => {
-        listAll(testReference).then(res => {
-            setlinks(res.items)
-            console.log(res.items)
-        })
-    }, [])
-
-
-    useEffect(() => {
-        if (links) getUrls()
-    }, [links])
-
-    const getUrls = async () => {
-        let urls = []
-        for (const imgPath of links) {
-            const pathReference = ref(storage, imgPath._location.path_)
-            const url = await getDownloadURL(pathReference)
-            urls.push(url)
-        }
-        setPath(urls)
-        console.log(urls)
-    }
-
-
+    const hallo = "test"
     return (
-        <>
-            {
-                path.map((url, i) => {
-                    return <img src={url} key={i} />
-                })
-            }
-        </>
+        <Iterate folder={hallo} />
     )
 }
