59acfc81b0f7990b29e67ce936e4e4142fe326b5
Tim Reber <tim.reber.1@postfinance.ch>
2022-03-14 16:23:54 +0100

    added logic data get from firebase

--- pages/test.js
+++ pages/test.js
@@ -1,9 +1,39 @@
 import Iterate from "components/Iterate";
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "firebase/clientAPP";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 
 export default function Test() {
+
+    const [folder, setFolder] = useState([])
+
+    const getData = async () => {
+        const querySnapshot = await getDocs(collection(db, "Folders"))
+        querySnapshot.forEach((doc) => {
+            const folderName = doc.data().FolderName
+            setFolder([...folder, folderName])
+            console.log(folderName)
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+
+
+
     const hallo = "test"
     return (
-        <Iterate folder={hallo} />
+        <>
+            {
+                folder.map((no, i) => {
+                    return <Iterate folder={folder[i]} />
+                })
+            }
+        </>
     )
 }
