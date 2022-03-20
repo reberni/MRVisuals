f9ad118a67f5e9e36ad25a6ede1481b6c580547c
PCNICOLA\Nicola Reber <nicola.reber@bluewin.ch>
2022-03-20 20:42:07 +0100

    Index cleanup & Added Header component

--- pages/folders/[folderName].js
+++ pages/folders/[folderName].js
@@ -1,15 +1,20 @@
 import Iterate from 'components/Iterate'
 import { useRouter } from 'next/router'
+import View from 'components/view'
 
 const Post = () => {
   const router = useRouter()
   const { folderName } = router.query
 
   return (
+    <>
+    <View text={folderName}/> 
+
     <div className='text-center'>
     <h1 className='text-2xl font-bold mb-8 mt-3'>{folderName}</h1>
     <Iterate folder={folderName} preview={false} />
     </div>
+    </>
   )
 }
 
