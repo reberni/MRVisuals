e4032de41a215275395640ffe9e45a7bda10245c
Nicola <nicola.reber@bluewin.ch>
2022-03-22 19:47:29 +0100

    CSS

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri"
+import { AiFillCamera } from "react-icons/ai"
+import { FaHammer, FaUserAlt } from "react-icons/fa"
+
 // components
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
+
 
 export default function Landing() {
   return (
@@ -30,8 +34,7 @@ export default function Landing() {
                   <h1 className="text-white font-semibold text-5xl">
                     MR Visuals
                   </h1>
-                  <p className="mt-12 text-lg text-blueGray-200">
-
+                  <p className="mt-12 text-lg text-white">
                         "THE CAMERA IS AN INSTRUMENT THAT TEACHES PEOPLE HOW TO SEE WITHOUT A CAMERA."<br/>
                     – DOROTHEA LANGE
                   </p>
@@ -42,31 +45,18 @@ export default function Landing() {
             className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
             style={{ transform: "translateZ(0)" }}
           >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
+
           </div>
         </div>
 
-        <section className="pb-20 bg-blueGray-200 -mt-24">
+        <section className="pb-20 bg-blueGray-200 mt-24">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap">
               <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
                     <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
-                      <i className="fas fa-hammer"></i>
+                    <FaHammer className="text-black text-2xl" />
                     </div>
                     <h6 className="text-xl font-semibold">Work</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
@@ -76,11 +66,11 @@ export default function Landing() {
                 </div>
               </div>
 
-              <div className="w-full md:w-4/12 px-4 text-center">
+              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
                     <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
-                      <i className="fas fa-camera"></i>
+                    <AiFillCamera className="text-black text-2xl" />
                     </div>
                     <h6 className="text-xl font-semibold">Gear</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
@@ -90,11 +80,11 @@ export default function Landing() {
                 </div>
               </div>
 
-              <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
                     <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
-                      <i className="fas fa-user"></i>
+                    <FaUserAlt className="text-black text-2xl" />
                     </div>
                     <h6 className="text-xl font-semibold">Social Media</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
@@ -137,27 +127,6 @@ export default function Landing() {
                     src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
                     className="w-full align-middle rounded-t-lg"
                   />
-                  <blockquote className="relative p-8 mb-4">
-                    <svg
-                      preserveAspectRatio="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 583 95"
-                      className="absolute left-0 w-full block h-95-px -top-94-px"
-                    >
-                      <polygon
-                        points="-30,95 583,95 583,65"
-                        className="text-blueGray-700 fill-current"
-                      ></polygon>
-                    </svg>
-                    <h4 className="text-xl font-bold text-white">
-                      Top Notch Services
-                    </h4>
-                    <p className="text-md font-light mt-2 text-white">
-                      The Arctic Ocean freezes every winter and much of the
-                      sea-ice then thaws every summer, and that process will
-                      continue whatever happens.
-                    </p>
-                  </blockquote>
                 </div>
               </div>
             </div>
